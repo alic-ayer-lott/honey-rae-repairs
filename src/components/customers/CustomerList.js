0abd1c43236d84f06ff22905896baa31ffdb7d36
Alicia AyersLott <amayers92@gmail.com>
2021-08-30 15:53:10 -0500

    began adding useEffect in CustomerList

--- src/components/customers/CustomerList.js
+++ src/components/customers/CustomerList.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react"; // importing React here so w
 export const CustomerList = () => { // defining and exporting component; this function should return JSX that shows in the browser
     const [customers, setCustomers] = useState([]) //invoking use state will return an array; why there is an array as the const on the left
         // setCustomers variables job is to hold function that modifies state for customers
+    const [totalCustomerMessage, updateMessage] = useState("")
     
     useEffect(
         () => {
@@ -16,6 +17,19 @@ export const CustomerList = () => { // defining and exporting component; this fu
         []
     ) // this is another hook bc it starts with use; a function we can invoke; takes 2 arguments; first is function second is always array
         //sole purpose of useEffect is to run certain code when state changes
+
+    useEffect(
+        () => {
+            if (customers.length === 1) {
+                updateMessage("You have 1 customer")
+            }
+            else {
+                updateMessage(`You have ${customers.length} customers`)
+            }
+        },
+        [customers]
+    )
+
     return ( //must have return statement here to generate the HTML
         <>
         {
